b845cd4c68591881352eb8d749daa155e3b9ae11
ahegde3 <ahegde3@outlook.com>
2024-12-29 19:41:53 -0500

    data for drop down fields added

--- src/utils/storage/UserData.ts
+++ src/utils/storage/UserData.ts
@@ -14,7 +14,7 @@ class UserData {
     // "Will you now or in the future require sponsorship for employment visa status? *":,  //SimpleDropdown
   };
 
-  public profileData = [
+  private profileData = [
     [
       0,
       {
@@ -51,13 +51,74 @@ class UserData {
         section: "",
       },
     ],
+    [
+      4,
+      {
+        answer: ["South Boston, Virginia, United States"],
+        fieldName: "Location (City) *",
+        fieldType: "SimpleDropdown",
+        section: "",
+      },
+    ],
+    [
+      5,
+      {
+        answer: ["Yes"],
+        fieldName: "Are you legally authorized to work in the United States? *",
+        fieldType: "SimpleDropdown",
+        section: "",
+      },
+    ],
+    [
+      6,
+      {
+        answer: ["No"],
+        fieldName:
+          "Will you now or in the future require sponsorship for employment visa status?",
+        fieldType: "SimpleDropdown",
+        section: "",
+      },
+    ],
+    [
+      7,
+      {
+        answer: ["Male"],
+        fieldName: "Gender",
+        fieldType: "SimpleDropdown",
+        section: "",
+      },
+    ],
+    [
+      8,
+      {
+        answer: ["No"],
+        fieldName: "Are you Hispanic/Latino?",
+        fieldType: "SimpleDropdown",
+        section: "",
+      },
+    ],
+    [
+      9,
+      {
+        answer: ["I am not a protected veteran"],
+        fieldName: "Veteran Status",
+        fieldType: "SimpleDropdown",
+        section: "",
+      },
+    ],
+    [
+      10,
+      {
+        answer: ["Asian"],
+        fieldName: "Please identify your race",
+        fieldType: "SimpleDropdown",
+        section: "",
+      },
+    ],
   ];
 
   public getUserProfileData = (): any => {
     return this.profileData;
-    // return Object.entries(this.data).map(([key, value], idx) => {
-    //   return [idx, { answer: value, fieldName: key }];
-    // });
   };
   public getFieldValue(fieldName: string): string {
     //find match if fieldName is not the key
